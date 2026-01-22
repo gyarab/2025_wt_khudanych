3cdc7db9e7bbada93845dd8a5b681b513c1eabbc
sergioGyarab <serhii.khudanych.s@gyarab.cz>
2026-01-22 14:34:56 +0100

    EXCHANGE PY

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,3 +1,7 @@
-import requests
 import httpx
-httpx.get("https://www.kurzy.cz/kurzy-men/kurzovni-listek/ceska-narodni-banka/")
+
+r = httpx.get("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt")
+print(r.text)
+print(r.status)
+lines = r.text.split('\n')
+print(lines[0])
