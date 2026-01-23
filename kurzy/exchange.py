1ab039635a037d3b74249797a452a5d4820a3838
sergioGyarab <serhii.khudanych.s@gyarab.cz>
2026-01-23 10:48:58 +0100

    Enhance exchange.py to display available currencies and improve user input handling; update requirements.txt to include colorama

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,11 +1,30 @@
 import httpx
+from colorama import Fore, Style
 
 url = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt"
 r = httpx.get(url)
 lines = r.text.split('\n')
 
-# Přeskočíme první 2 řádky (datum a hlavičku) a jdeme rovnou na data
-for i in lines[2:]:
-    if i:  # Kontrola, zda řádek není prázdný (na konci souboru bývá prázdný řádek)
+vyber_meny = input("Vyberte měnu pro převod na CZK (podle ISO 4217 standardu, 3 místný kód), pro zobrazení dostupných měn stiskněte Enter: ")
+if vyber_meny == "":
+    print("Dostupné měny:")
+    for i in lines[2:]:
+        if i.strip() == "":
+            continue
+        parts = i.split('|')
+        print(f"{parts[3]} ({parts[0]})")
+    vyber_meny = input("Zadejte kód měny pro převod na CZK: ")
+vybrana_mena = vyber_meny.upper()
+line_mena = ""
+
+for i in lines:
+    if vybrana_mena in i: 
         parts = i.split('|')  
-        print(parts[0], parts[3], parts[4] + " CZK")
+        line_mena = i
+        break
+
+castka = float(input(f"Zadejte částku v {vybrana_mena} pro převod na CZK: "))
+castka_vybrane_meny = float(line_mena.split('|')[2].replace(',', '.'))
+prevod = float(line_mena.split('|')[4].replace(',', '.')) / castka_vybrane_meny * castka
+POCET_DESETINNYCH_MIST = 2
+print(Fore.BLUE + str(prevod.__round__(POCET_DESETINNYCH_MIST)) + " CZK" + Style.RESET_ALL)
